209064718671b594c22a23dd684744142b9dafb9
Michael Kim <mike02kim@gmail.com>
2022-09-17 12:44:52 -0400

    change it to trash2treasure

---
+++ CS50-Web/search/navigation.js
@@ -0,0 +1,19 @@
+function toBuy() {
+    window.location.href = "buy.html"
+}
+
+function toHome() {
+    window.location.href = "home.html"
+}
+
+function toSell() {
+    window.location.href = "sell.html"
+}
+
+function toLogin() {
+    window.location.href = "login.html"
+}
+
+function toList() {
+    document.getElementById("demo").innerHTML = "your text here";
+}
