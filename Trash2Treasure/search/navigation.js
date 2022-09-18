7e8b06893cc0f766068097712a71d2a390162cfe
Michael Kim <mike02kim@gmail.com>
2022-09-17 23:00:04 -0400

    change files

---
+++ Trash2Treasure/search/navigation.js
@@ -0,0 +1,25 @@
+
+function toBuy() {
+    window.location.href = "buy.html"
+}
+
+function toHome() {
+    window.location.href = "home.html"
+}
+
+function toSell() {
+    window.location.href = "listing.html"
+}
+
+function toLogin() {
+    window.location.href = "login.html"
+}
+
+function toList() {
+    var x = document.getElementById("itemCatagory");
+    alert(x);
+}
+
+function toForms() {
+    window.location.href= "forms.html"
+}
